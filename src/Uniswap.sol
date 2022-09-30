cdd34d8e66d56943fde8b2174f753b39cbecd485
rottaj <jackrotta12@gmail.com>
2022-09-30 12:14:15 -0400

    Pushing changes

--- src/Uniswap.sol
+++ src/Uniswap.sol
@@ -31,6 +31,8 @@ contract Uniswap {
 
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant AAVE = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
+
 
     address public constant UniswapRouter2 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
@@ -41,6 +43,7 @@ contract Uniswap {
 
     IERC20 dai = IERC20(DAI);
     IERC20 usdc = IERC20(USDC);
+    IERC20 aave = IERC20(AAVE);
 
     function swapUsdcToDai( 
         uint _amountIn, 
@@ -62,7 +65,27 @@ contract Uniswap {
     }
 
 
-    function getAmountOut(
+    function swapAaveToUsdc( 
+        uint _amountIn, 
+        uint _amountOutMin, 
+        address[] memory _path,
+        address _to, 
+        uint _deadline
+    ) public returns (uint[] memory) {
+        // Approve ERC20 Token to Uniswap Router02 
+        aave.approve(UniswapRouter2, _amountIn);
+        router.swapExactTokensForTokens( // Get amounts [USDC, DAI]
+            _amountIn, 
+            _amountOutMin, 
+            _path, 
+            _to, 
+            _deadline
+        );
+        //emit _Swap(_path, _to);
+    }
+
+
+    function getAmountsOut(
         uint _amountIn, 
         address[] memory s_path // list of token addresses
     ) public returns (uint) {
@@ -78,6 +101,11 @@ contract Uniswap {
 
     function getBalanceUsdc() public view returns (uint256) {
         uint256 balance = usdc.balanceOf(address(this));
+        return balance;
+    }
+
+    function getBalanceAave() public view returns (uint256) {
+        uint256 balance = aave.balanceOf(address(this));
         return balance;
     }
 
--- test/Uniswap.t.sol
+++ test/Uniswap.t.sol
@@ -43,14 +43,26 @@ contract UniswapTestTester is Test {
     function testTokenBalances() public {
         uint256 balanceDai = testContract.getBalanceDai();
         uint256 balanceUsdc = testContract.getBalanceUsdc();
-        assertEq(balanceDai, 1000000000000000000000); // Expect to have 1000 Dai
-        assertEq(balanceUsdc, 1000000000000000000000); // Expect to have 1000 USDC
+        uint256 balanceAave = testContract.getBalanceAave();
+        assertEq(balanceAave, (1000 * 1e18)); // Expect to have 1000 USDC   
+        assertEq(balanceUsdc, (1000 * 1e18)); // Expect to have 1000 USDC   
+        assertEq(balanceDai, (1000 * 1e18)); // Expect to have 1000 USDC   
 
     }
 
     function testRouter() public {
         address routerAddress = testContract.getRouter();
         assertEq(routerAddress, address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D));
+    }
+
+    // Calculate Optimal token amounts before calling Swap()
+    function testAmountsOutAaveToUsdc() public {
+        address[] memory s_tokens = new address[](2);
+        s_tokens[0] = address(AAVE); // Add some logs here
+        s_tokens[1] = address(USDC);
+        //uint amounts = testContract.getAmountOut((1 * 1e18), s_tokens); 
+        uint amounts = testContract.getAmountsOut(1 * 1e18, s_tokens); 
+        assertEq(amounts, 63987204); // Returns in Gwei
     }
 
 
@@ -61,40 +73,40 @@ contract UniswapTestTester is Test {
         s_tokens[0] = address(USDC); // Add some logs here
         s_tokens[1] = address(DAI);
         testContract.swapUsdcToDai(  
-            10000, // Amount In
-            1000, // Amount Out Min
+            (100 * 1e18), // Amount In
+            (1 * 1e18), // Amount Out Min
             s_tokens, // Path [USDC, DAI]
             address(this), // This address
-            block.timestamp + 1 minutes // Deadline
+            block.timestamp + 1 hours // Deadline
         );
     }
 
-    // Calculate Optimal token amounts before calling Swap()
-    function testAmountsOutAaveToUsdc() public {
-        address[] memory s_tokens = new address[](2);
-        s_tokens[0] = address(AAVE); // Add some logs here
-        s_tokens[1] = address(USDC);
-        uint amounts = testContract.getAmountOut((1 * 1e18), s_tokens);
-        assertEq(amounts, 1);
-    }
+    function testSwapAaveToUsdc() public {
 
+        address[] memory s_tokenst = new address[](2);
+        s_tokenst[0] = address(AAVE); // Add some logs here
+        //s_tokens[0] = address(AAVE);
+        s_tokenst[1] = address(USDC);
 
-    function testTokenBalancesAfterSwap() public {
-        address[] memory s_tokens = new address[](2);
-        s_tokens[0] = address(USDC); // Add some logs here
-        s_tokens[1] = address(DAI);
-        testContract.swapUsdcToDai(  
-            10000, // Amount In
-            1000, // Amount Out Min
-            s_tokens, // Path [USDC, DAI]
+        testContract.swapAaveToUsdc(  
+            (1 * 1e18), // Amount In
+            (1000 * 1e18), // Amount Out Min
+            s_tokenst, // Path [USDC, DAI]
             address(this), // This address
             block.timestamp + 1 minutes // Deadline
         );
+
+
+    }
+
+    function testBalancesAfterSwaps() public {
         uint256 balanceDai = testContract.getBalanceDai();
         uint256 balanceUsdc = testContract.getBalanceUsdc();
-        assertEq(balanceDai, 100000000000000000000); // Expect to have 1000 Dai
-        assertEq(balanceUsdc, 100000000000000000000); // Expect to have 1000 USDC
+        uint256 balanceAave = testContract.getBalanceAave();
 
+        assertEq(balanceAave, (1000 * 1e18)); // Expect to have 1000 USDC   
+        assertEq(balanceUsdc, (1000 * 1e18)); // Expect to have 1000 USDC   
+        assertEq(balanceDai, (1000 * 1e18)); // Expect to have 1000 USDC   
     }
 
 }
